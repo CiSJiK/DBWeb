73b7ef8957001e3dee454ecd74de7dd16a82364b
CiSJiK <sj77007983@kangwon.ac.kr>
2023-06-20 03:41:21 +0900

    update the commit;

--- src/main/java/knucse/domain/Novel.java
+++ src/main/java/knucse/domain/Novel.java
@@ -1,33 +1,16 @@
 package knucse.domain;
-import jakarta.persistence.*;
-@Entity
-public class novel {
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int nnum;
-    private String nname;
-    private String ncon;
+import lombok.Data;
 
-    public int getNnum() {
-        return nnum;
+public class Novel {
+    @Data
+    public static class Create{
+        private String nname;
+        private String ncon;
     }
-
-    public void setNnum(int nnum) {
-        this.nnum = nnum;
-    }
-
-    public String getNname() {
-        return nname;
-    }
-
-    public void setNname(String nname) {
-        this.nname = nname;
-    }
-
-    public String getNcon() {
-        return ncon;
-    }
-
-    public void setNcon(String ncon) {
-        this.ncon = ncon;
+    @Data
+    public static class Simple{
+        private int nnum;
+        private String nname;
+        private String ncon;
     }
 }
